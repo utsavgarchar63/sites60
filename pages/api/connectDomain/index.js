27ae46043cf6b72c49dda37f287afc1ac71c8fc9
Utsav Garchar <130067889+utsavgarchar63@users.noreply.github.com>
2024-10-12 13:51:16 +0530

    Update index.js

--- pages/api/connectDomain/index.js
+++ pages/api/connectDomain/index.js
@@ -75,7 +75,19 @@ export default async function handler(req, res) {
 
                             // Create a new GoogleAuth instance
                             const auth = new GoogleAuth({
-                                keyFile: keyFilePath,
+                                keyFile: {
+     																type: "service_account",
+																     project_id: process.env.PROJECT_ID,
+																     private_key_id: process.env.PRIVATE_KEY_ID,
+																     private_key: process.env.PRIVATE_KEY,
+																     client_email: process.env.CLIENT_EMAIL,
+																     client_id: process.env.CLIENT_ID,
+																     auth_uri: process.env.AUTH_URI,
+																     token_uri: process.env.TOKEN_URI,
+																     auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+																     client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+																     universe_domain: process.env.UNIVERSE_DOMAIN
+																},
                                 scopes: ['https://www.googleapis.com/auth/compute'],
                             });
 
@@ -167,7 +179,7 @@ export default async function handler(req, res) {
                                             "name": sslCertificateName,
                                             "networkTier": "PREMIUM"
                                         };
-                    
+                  
                                         const ipAddressResponse = await fetch(ipAddressUrl, {
                                             method: 'POST',
                                             headers: {
